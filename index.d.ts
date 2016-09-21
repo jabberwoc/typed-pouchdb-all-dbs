8347e577cd23e1345f16f49f97d000a131f92130
Nao Ohira <nao.ohira@aareon.fr>
2016-09-21 09:27:51 +0200

    Add function export

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
-namespace PouchDB {
+declare module 'pouchdb-all-dbs' {
+    const pouchdbAllDbs : (pouchDB : PouchDB.Static) => void;
+    export = pouchdbAllDbs;
+}
+
+declare namespace PouchDB {
     interface Static {
         allDbs(callback : (err : any, dbs : string[]) => void) : void;
         allDbs() : Promise<string[]>;
