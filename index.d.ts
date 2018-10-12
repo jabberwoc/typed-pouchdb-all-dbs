66dcbbffa63c79283b2f8c559989447f98c9b323
Sebastian Stöcker <s.stoecker@surfhome.de>
2018-10-12 19:46:35 +0200

    declare plugin

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,17 @@
-declare module 'pouchdb-all-dbs' {
-    const pouchdbAllDbs : (pouchDB : PouchDB.Static) => void;
-    export = pouchdbAllDbs;
-}
-
+/// <reference types="pouchdb-core" />
+  
 declare namespace PouchDB {
     interface Static {
-        allDbs(callback : (err : any, dbs : string[]) => void) : void;
-        allDbs() : Promise<string[]>;
-
-        resetAllDbs(callback : (err : any) => void) : void;
-        resetAllDbs() : Promise<void>;
+      allDbs(callback: (err: any, dbs: string[]) => void): void;
+      allDbs(): Promise<string[]>;
+  
+      resetAllDbs(callback: (err: any) => void): void;
+      resetAllDbs(): Promise<void>;
     }
-}
+  }
+
+  declare module 'pouchdb-all-dbs' {
+    const plugin: PouchDB.Plugin;
+    export = plugin;
+  }
+  
